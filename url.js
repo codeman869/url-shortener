4b617388b30a8d29dafa5a4ee4971c1eef9029a4
Cody Deckard <cjdeckard@gmail.com>
2017-02-22 21:18:58 +0000

    Installs URL-Validator and Error Response
    Installs url-validator and updates the url response to
    check if a valid url is passed. If not an error message will
    be returned to the client.

--- url.js
+++ url.js
@@ -1,7 +1,19 @@
+'use strict'
+const validator = require('url-validator');
+
 exports.shortenURL = (req, res) => {
+    let url = validator(req.params.url);
+    
+    if(!url) {
+      res.json({
+          error: 'Wrong URL format'
+      });  
+    } 
+    
+    
     
 };
 
-exports.viewURL = () => {
+exports.viewURL = (req,res) => {
     
 };
